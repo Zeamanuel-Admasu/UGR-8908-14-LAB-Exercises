00ab2a2a7de4609dde0dea1400ae513861345904
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2024-03-05 16:25:05 +0300

    Lab-5-functions

---
+++ LAB-5-functions.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  int curr = 0;
+  for (int i = 0; i < numbers.length; i++) {
+    curr = curr + numbers[i];
+  }
+  print(curr);
+}

5efdef20d9511bf737f5b6c044fa5120fdf102aa
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2024-04-23 23:05:10 +0300

    bloc

--- StateManagementAndNetwork/blockpost/block.dart
+++ StateManagementAndNetwork/blockpost/block.dart
@@ -1,146 +1,127 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
-
-class Post {
-  final int id;
-  final String title;
-  final String body;
-
-  Post({required this.id, required this.title, required this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-    );
-  }
-}
-
-abstract class PostEvent {}
-
-class FetchPosts extends PostEvent {}
-
-abstract class PostState {}
-
-class PostInitial extends PostState {}
-
-class PostLoading extends PostState {}
-
-class PostLoaded extends PostState {
-  final List<Post> posts;
-  PostLoaded({required this.posts});
-}
-
-class PostError extends PostState {}
-
-class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(PostInitial()) {
-    on<FetchPosts>((event, emit) async {
-      emit(PostLoading());
-      try {
-        final response = await http
-            .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
-        if (response.statusCode == 200) {
-          final List<dynamic> data = jsonDecode(response.body);
-          final List<Post> posts =
-              data.map((json) => Post.fromJson(json)).toList();
-          emit(PostLoaded(posts: posts));
-        } else {
-          emit(PostError());
-        }
-      } catch (e) {
-        emit(PostError());
-      }
-    });
-  }
-}
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Todo App',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: BlocProvider(
-        create: (context) => PostBloc()..add(FetchPosts()),
-        child: const MyHomePage(),
+        create: (context) => TodoBloc()..add(FetchTodos()),
+        child: TodoScreen(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class Todo {
+  final int id;
+  final String title;
+  final bool completed;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+}
+
+abstract class TodoEvent {}
+
+class FetchTodos extends TodoEvent {}
+
+abstract class TodoState {}
+
+class TodoLoading extends TodoState {}
+
+class TodoLoaded extends TodoState {
+  final List<Todo> todos;
+
+  TodoLoaded(this.todos);
+}
+
+class TodoError extends TodoState {
+  final String message;
+
+  TodoError(this.message);
+}
+
+class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  TodoBloc() : super(TodoLoading());
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Posts'),
-        backgroundColor: Color.fromARGB(255, 230, 226, 19),
-      ),
-      body: Container(
-        color: Colors.blue,
-        child: BlocBuilder<PostBloc, PostState>(
-          builder: (context, state) {
-            if (state is PostInitial || state is PostLoading) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is PostLoaded) {
-              return ListView.separated(
-                separatorBuilder: ((context, index) =>
-                    const SizedBox(height: 5)),
-                itemCount: state.posts.length,
-                itemBuilder: (context, index) {
-                  final post = state.posts[index];
-                  return ListTile(
-                    tileColor: const Color.fromARGB(255, 0, 0, 0),
-                    title: Text(post.title),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PostDetailsPage(post: post),
-                        ),
-                      );
-                    },
-                  );
-                },
-              );
-            } else {
-              return const Center(child: Text('Something went wrong!'));
-            }
-          },
-        ),
-      ),
-    );
+  Stream<TodoState> mapEventToState(TodoEvent event) async* {
+    if (event is FetchTodos) {
+      yield* _mapFetchTodosToState();
+    }
+  }
+
+  Stream<TodoState> _mapFetchTodosToState() async* {
+    yield TodoLoading();
+    try {
+      final todos = await _fetchTodos();
+      yield TodoLoaded(todos);
+    } catch (e) {
+      yield TodoError('Failed to fetch todos');
+    }
+  }
+
+  Future<List<Todo>> _fetchTodos() async {
+    final apiUrl = Uri.parse('https://mocki.io/v1/your-fake-api-url');
+    final response = await http.get(apiUrl);
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = json.decode(response.body);
+      return jsonList
+          .map((json) => Todo(
+                id: json['id'],
+                title: json['title'],
+                completed: json['completed'],
+              ))
+          .toList();
+    } else {
+      throw Exception('Failed to load todos');
+    }
   }
 }
 
-class PostDetailsPage extends StatelessWidget {
-  final Post post;
-
-  const PostDetailsPage({super.key, required this.post});
-
+class TodoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(post.title),
+      appBar: AppBar(title: Text('Todo List')),
+      body: BlocBuilder<TodoBloc, TodoState>(
+        builder: (context, state) {
+          if (state is TodoLoading) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is TodoLoaded) {
+            return ListView.builder(
+              itemCount: state.todos.length,
+              itemBuilder: (context, index) {
+                final todo = state.todos[index];
+                return ListTile(
+                  title: Text(todo.title),
+                  subtitle: Text('Completed: ${todo.completed}'),
+                );
+              },
+            );
+          } else if (state is TodoError) {
+            return Center(child: Text(state.message));
+          }
+          return Container(); // Placeholder widget
+        },
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(post.body),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<TodoBloc>().add(FetchTodos());
+        },
+        child: Icon(Icons.refresh),
       ),
     );
   }
